647337e3984d64b8f1a006a49266ebb8f165057c
stardragon100 <joelmanuelcj@gmail.com>
2023-07-18 13:49:15 +0530

    info updated

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -30,53 +30,38 @@ class Info extends Component{
 
 render(){
   return (
-    <div className='details'>
+    <div className='detail'>
     <div className='info-content'>
-    <div className='algorithm'>
-    {/* <h1 className='name'>Linear Regression</h1> */}
-        <button className='infobutton' value="Linear Regression" onClick={(e)=>{this.getDetails(e)}}>Linear Regression</button>
-        {/* <a href="https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LinearRegression.html"><button className='infobutton'>Parameters</button></a> */}
-    </div>
-    <div className='algorithm'>
-    {/* <h1 className='name'>Logistic Regression</h1> */}
-        <button className='infobutton' value="Logistic Regression" onClick={(e)=>{this.getDetails(e)}}>Logistic Regression</button>
-        {/* <a href="https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html"><button className='infobutton'>Parameters</button></a> */}
-      </div>
-    <div className='algorithm'>
-    {/* <h1 className='name'>SVM</h1> */}
-        <button className='infobutton' value="SVM" onClick={(e)=>{this.getDetails(e)}}>SVM</button>
-        {/* <a href="https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html"><button className='infobutton'>Parameters</button></a> */}
+      <div className='algorithm'>
+          <button className='infobutton' value="Linear Regression" onClick={(e)=>{this.getDetails(e)}}>Linear Regression</button>
       </div>
       <div className='algorithm'>
-    {/* <h1 className='name'>Decision Tree</h1> */}
-    <button className='infobutton' value="Decision Tree" onClick={(e)=>{this.getDetails(e)}}>Decision Tree</button>
-        {/* <a href="https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html"><button className='infobutton'>Parameters</button></a> */}
-      </div>
+          <button className='infobutton' value="Logistic Regression" onClick={(e)=>{this.getDetails(e)}}>Logistic Regression</button>
+        </div>
+      <div className='algorithm'>
+          <button className='infobutton' value="SVM" onClick={(e)=>{this.getDetails(e)}}>SVM</button>
+        </div>
+        <div className='algorithm'>
+      <button className='infobutton' value="Decision Tree" onClick={(e)=>{this.getDetails(e)}}>Decision Tree</button>
+        </div>
 
-      <div className='algorithm'>
-    {/* <h1 className='name'>Random Forest</h1>
-    <h2 className='name1'>Classifier</h2> */}
-        <button className='infobutton' value="Random Forest" onClick={(e)=>{this.getDetails(e)}}>Randon Forest</button>
-        {/* <a href="https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html"><button className='infobutton'>Parameters</button></a> */}
-    {/* <h2 className='name1'>Regressor</h2>
-        <a href="https://towardsdatascience.com/random-forest-regression-5f605132d19d"><button className='infobutton'>Algorithm</button></a>
-        <a href="https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestRegressor.html"><button className='infobutton'>Parameters</button></a>  */}
-      </div>
+        <div className='algorithm'>
+    
+          <button className='infobutton' value="Random Forest" onClick={(e)=>{this.getDetails(e)}}>Randon Forest</button>
+          
+        </div>
 
-      <div className='algorithm'>
-    {/* <h1 className='name'>KMeans</h1> */}
-        <button className='infobutton' value="KMeans" onClick={(e)=>{this.getDetails(e)}}>KMeans</button>
-        {/* <a href="https://scikit-learn.org/stable/modules/generated/sklearn.cluster.KMeans.html"><button className='infobutton'>Parameters</button></a> */}
-      </div>
+        <div className='algorithm'>
+          <button className='infobutton' value="KMeans" onClick={(e)=>{this.getDetails(e)}}>KMeans</button>
+        </div>
 
-      <div className='algorithm'>
-      <button className='infobutton' value="KNN" onClick={(e)=>{this.getDetails(e)}}>KNN</button>
-      </div>
+        <div className='algorithm'>
+        <button className='infobutton' value="KNN" onClick={(e)=>{this.getDetails(e)}}>KNN</button>
+        </div>
 
-      <div className='algorithm'>
-    {/* <h1 className='name'>Naive Bayes</h1> */}
-      <button className='infobutton' value="Naive Bayes" onClick={(e)=>{this.getDetails(e)}}>Naive Bayes</button>
-      </div>
+        <div className='algorithm'>
+        <button className='infobutton' value="Naive Bayes" onClick={(e)=>{this.getDetails(e)}}>Naive Bayes</button>
+        </div>
       </div>
       <div>
       <textarea className='details' value={this.state.details}>
